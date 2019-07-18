e3b463fd96d37a1b293c02f233c8bef39aecae89
daiyi-guizhou <847210821@qq.com>
2019-07-19 02:04:17 +0800

    20190719

--- monitor--dashboard---agent/falcon-dashboard/retart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo docker stop falcon-dashboard-self && sudo docker rm falcon-dashboard-self && sudo docker build . -t falcon-dashboard:local && bash start
-
-
--- monitor--dashboard---agent/prometheus---agent/deb/home/work/prometheus/push-scripts/ping_check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if ping -c 1 $1 >/dev/null 2>&1
-then 
-    # echo True
-    echo 1
-else 
-    # echo Flase
-    echo 0
-fi
